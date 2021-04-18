01506b9e142d3ba5e014d4628cf083f6edce763e
chenxujian616 <13922606186@139.com>
2021-04-18 16:29:56 +0800

    router files

---
+++ routes/emqx_web_hook.js
@@ -0,0 +1,10 @@
+/* IotHubServer/routes/emqx_web_hook.js */
+var express = require('express')
+var router = express.Router()
+
+router.post("/", function (req, res) {
+    console.log(req.body)
+    res.status(200).send("ok")
+})
+
+module.exports = router
